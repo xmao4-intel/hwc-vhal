a2aa14ea15330af92e8a16b630ff8722f9bff260
renchenglei <chenglei.ren@intel.com>
2021-08-03 11:23:00 +0800

    Add license header & COPYING license files
    Add copying files and Apache 2.0 license header
    
    Signed-off-by: Chenglei Ren <chenglei.ren@intel.com>

--- uio/UioDisplay.h
+++ uio/UioDisplay.h
@@ -1,3 +1,28 @@
+/*
+Copyright (C) 2021 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions
+and limitations under the License.
+
+
+SPDX-License-Identifier: Apache-2.0
+
+Author:
+Ren Chenglei (chenglei.ren@intel.com)
+Tang Shaofeng (shaofeng.tang@intel.com)
+Date: 2021.06.09
+
+*/
+
 #ifndef __UIO_DISPLAY__H
 #define __UIO_DISPLAY__H
 
